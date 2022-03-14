7a9553e3237c4537b5827c29c4b152dbaf336b40
Dhanu0510 <dhananjaypatel.official@gmail.com>
2022-03-14 17:42:44 +0530

    created API for order

--- base/views/user_views.py
+++ base/views/user_views.py
@@ -37,18 +37,17 @@ class MyTokenObtainPairView(TokenObtainPairView):
 def registerUser(request):
     data = request.data
     try:
-        user = User.objects.create_user(
-            first_name=data["name"],
-            username=data["email"],
-            email=data["email"],
-            password=make_password(data["password"]),
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
         )
+
         serializer = UserSerializerWithToken(user, many=False)
         return Response(serializer.data)
     except:
-        message = {
-            "detail": "User already exists"
-        }
+        message = {'detail': 'User with this email already exists'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
